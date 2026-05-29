a750df98de70a82e08613e188ecac4270df3c965
Valentine Rahier <valentine.rahier@inrae.fr>
2026-05-29 12:10:00 +0200

    feat: add balance closure test

---
+++ R/balance_closure.R
@@ -0,0 +1,70 @@
+#' @export
+BalanceClosureTest <- R6::R6Class("BalanceClosureTest",  # nolint: object_name_linter
+  private = list(
+    config = NULL
+  ),
+  public = list(
+    initialize = function(config) {
+      private$config <- config
+    },
+    run = function() {
+      usms <- list.files(private$config$usms_workspace)
+      if (!is.null(private$config$usms)) {
+        usms <- intersect(usms, private$config$usms)
+      }
+      logger::log_info(
+        "Running balance closure test on ",
+        length(usms),
+        " USMs..."
+      )
+      error_usms <- NULL
+      logger::log_debug("Loading simulations data for balance closure test...")
+      sim_list <- SticsRFiles::get_sim(
+        workspace = private$config$usms_workspace,
+        usm = usms,
+        verbose = is_debug(),
+        parallel = private$config$parallel,
+        cores = private$config$cores
+      )
+      for (usm in names(sim_list)) {
+        sim <- sim_list[[usm]]
+        balances <- c(
+          "H2O_balance", "plant_N_balance", "soil_mineral_N_balance",
+          "soil_organic_N_balance", "soil_organic_C_balance"
+        )
+        for (balance in balances) {
+          balance_fields <- c(
+            paste0("init_", balance), paste0("final_", balance)
+          )
+          if (all(balance_fields %in% names(sim))) {
+            latest_sim <- sim[which.max(sim$Date), ]
+            if (all(is.na(latest_sim[balance_fields]))) {
+              logger::log_debug(
+                "USM ", usm,
+                ": balance fields are present but empty, skipping..."
+              )
+              next
+            }
+            init_balance <- round(latest_sim[[paste0("init_", balance)]])
+            final_balance <- round(latest_sim[[paste0("final_", balance)]])
+            if (
+              !is.na(init_balance) && !is.na(final_balance) &&
+                init_balance != final_balance
+            ) {
+              logger::log_warn(
+                "USM ", usm, ": ", balance, " balance closure failed (init: ",
+                init_balance, ", final: ", final_balance, ")"
+              )
+              error_usms <- c(error_usms, usm)
+            } else {
+              logger::log_debug(
+                "USM ", usm, ": ", balance, " balance closure passed (init: ",
+                init_balance, ", final: ", final_balance, ")"
+              )
+            }
+          }
+        }
+      }
+    }
+  )
+)
